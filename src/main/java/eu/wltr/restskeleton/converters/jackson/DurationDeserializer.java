feb0fcb81ad065a430fceb5686ab58b4e626177e
Sven Walter <sven@wltr.eu>
2013-06-29 22:02:12 +0200

    Made Duration serialization XML duration like.

--- src/main/java/eu/wltr/restskeleton/converters/jackson/DurationDeserializer.java
+++ src/main/java/eu/wltr/restskeleton/converters/jackson/DurationDeserializer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.DeserializationContext;
@@ -20,24 +19,65 @@ public class DurationDeserializer extends JsonDeserializer<Duration>
 		implements
 			JacksonConverter {
 
+	public final static Pattern pattern = Pattern
+			.compile("^P((\\d+)Y)?((\\d+)M)?((\\d+)D)?(T((\\d+)H)?((\\d+)M)?((\\d+)S)?)?$");
+
+	public final static int POS_YEARS = 2;
+	public final static int POS_MONTHS = 4;
+	public final static int POS_DAYS = 6;
+	public final static int POS_HOURS = 9;
+	public final static int POS_MINUTES = 11;
+	public final static int POS_SECONDS = 13;
+
 	@Override
 	public void register(SimpleModule module) {
 		module.addDeserializer(Duration.class, new DurationDeserializer());
 
 	}
 
+	private long getLong(Matcher m, int pos) {
+		try {
+			return Long.parseLong(m.group(pos));
+		} catch (NumberFormatException e) {
+			return 0L;
+		}
+	}
+
 	@Override
 	public Duration deserialize(JsonParser jsonParser,
 			DeserializationContext deserializationContext) throws IOException,
 			JsonProcessingException {
-		Matcher m = Pattern.compile("^s([0-9]+)$")
-				.matcher(jsonParser.getText());
+		String text = jsonParser.getText();
+		Matcher m = pattern.matcher(text);
 
-		if (m.matches())
-			return new Duration(Integer.parseInt(m.group(1)));
-		else
-			throw new JsonGenerationException("Invalid Duration format.");
+		if (m.matches()) {
+			long years = getLong(m, POS_YEARS);
+			long months = getLong(m, POS_MONTHS);
+			long days = getLong(m, POS_DAYS);
+			long hours = getLong(m, POS_HOURS);
+			long minutes = getLong(m, POS_MINUTES);
+			long seconds = getLong(m, POS_SECONDS);
+
+			long result = 0;
+
+			result += years;
+			result *= 12;
+			result += months;
+			result *= 30;
+			result += days;
+			result *= 24;
+			result += hours;
+			result *= 60;
+			result += minutes;
+			result *= 60;
+			result += seconds;
+
+			return new Duration(result);
+
+		} else {
+			return new Duration(Long.parseLong(text));
+
+		}
 
 	}
-
 }
--- src/main/java/eu/wltr/restskeleton/converters/jackson/DurationSerializer.java
+++ src/main/java/eu/wltr/restskeleton/converters/jackson/DurationSerializer.java
@@ -27,7 +27,48 @@ public class DurationSerializer extends JsonSerializer<Duration>
 	public void serialize(Duration value, JsonGenerator jgen,
 			SerializerProvider provider) throws IOException,
 			JsonGenerationException {
-		jgen.writeString(String.format("s%s", value.seconds));
+
+		long remain = value.seconds;
+
+		long seconds = remain % 60;
+		remain /= 60;
+
+		long minutes = remain % 60;
+		remain /= 60;
+
+		long hours = remain % 24;
+		remain /= 24;
+
+		long days = remain % 30;
+		remain /= 30;
+
+		long months = remain % 12;
+		remain /= 12;
+
+		String string = "";
+
+		if (seconds != 0)
+			string = seconds + "S";
+
+		if (minutes != 0)
+			string = minutes + "M" + string;
+
+		if (hours != 0)
+			string = hours + "H" + string;
+
+		if (!string.isEmpty())
+			string = "T" + string;
+
+		if (days != 0)
+			string = days + "D" + string;
+
+		if (months != 0)
+			string = months + "M" + string;
+
+		if (remain != 0)
+			string = remain + "Y" + string;
+
+		jgen.writeString("P" + string);
 
 	}
 }
