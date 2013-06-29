cf0cd4ed41d636760121fb73cdf3316d6db039c5
Sven Walter <sven@wltr.eu>
2013-06-29 17:37:28 +0200

    Simplified Jackson mapper.

--- src/main/java/eu/wltr/restskeleton/fields/DurationSerializer.java
+++ src/main/java/eu/wltr/restskeleton/fields/DurationSerializer.java
@@ -1,33 +1,19 @@
 package eu.wltr.restskeleton.fields;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.SerializerProvider;
-import org.codehaus.jackson.map.ser.SerializerBase;
 
-public class DurationSerializer extends SerializerBase<Duration> {
-	public DurationSerializer() {
-		super(Duration.class);
-
-	}
+public class DurationSerializer extends JsonSerializer<Duration> {
 
 	@Override
 	public void serialize(Duration value, JsonGenerator jgen,
 			SerializerProvider provider) throws IOException,
 			JsonGenerationException {
 		jgen.writeString(String.format("s%s", value.seconds));
-
-	}
-
-	@Override
-	public JsonNode getSchema(SerializerProvider provider, Type typeHint)
-			throws JsonMappingException {
-		throw new UnsupportedOperationException();
 
 	}
 }
--- src/main/java/eu/wltr/restskeleton/models/TimeDocument.java
+++ src/main/java/eu/wltr/restskeleton/models/TimeDocument.java
@@ -3,6 +3,7 @@ package eu.wltr.restskeleton.models;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import eu.wltr.restskeleton.fields.Duration;
@@ -13,6 +14,7 @@ public class TimeDocument {
 	@Id
 	public String id;
 
+	@Indexed
 	public String name;
 
 	public Date date;
