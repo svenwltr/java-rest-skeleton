faa7845a05368245443b2f47ab3e54aceaa052c2
Sven Walter <sven@wltr.eu>
2013-06-30 17:20:36 +0200

    UI update.

--- src/main/webapp/WEB-INF/static/js/models/time.js
+++ src/main/webapp/WEB-INF/static/js/models/time.js
@@ -1,86 +1,109 @@
-define([ "jquery", "backbone", "text!html/time.tpl.html", "utils/templating" ], function($,
-		Backbone, template, Templating) {
+define([ "jquery", "backbone", "text!html/time.tpl.html", "utils/templating" ],
+		function($, Backbone, template, Templating) {
 
-	var Time = Backbone.Model.extend({
+			var Time = Backbone.Model.extend({
 
-		defaults : function() {
-			return {
-				name : null,
-				date : Date.now(),
-				duration : null
-			};
-		}
+				defaults : function() {
+					return {
+						name : null,
+						date : Date.now(),
+						duration : null
+					};
+				}
 
-	});
-
-	var TimeList = Backbone.Collection.extend({
-		model : Time,
-		url : '/rest/time/'
-			
-	});
-
-	var timeList = new TimeList;
-	
-
-	var TimeView = Backbone.View.extend({
-		template : Templating.extract($(template), "item"),
-
-		initialize : function() {
-			this.el = this.template.clone();
-			
-			this.listenTo(this.model, 'change', this.render);
-
-		},
-
-		render : function() {
-			Templating.render(this.el, this.model.toJSON());
-			return this;
-			
-		}
-
-	});
-
-	var TimeListView = Backbone.View.extend({
-
-		template : Templating.extract($(template), "timetable-list"),
-
-		initialize : function() {
-			this.listenTo(timeList, 'add', this.addOne);
-			this.listenTo(timeList, 'all', this.render);
-			
-			this.el = this.template.clone();
-
-			$("#timetable-list").replaceWith(this.el);
-			
-			timeList.fetch();
-
-		},
-
-		render : function() {
-			Templating.render(this.el, {items: []});
-			
-		},
-
-		addOne : function(time) {
-			var view = new TimeView({
-				model : time
 			});
-			
-			Templating.append(this.el, "items", view.render().el);
-			
-		}
 
-	});
-	
-	var TimeFormView = Backbone.View.extend({
-		
-	});
+			var TimeList = Backbone.Collection.extend({
+				model : Time,
+				url : '/rest/time/'
 
-	return {
-		init : function() {
-			return new TimeListView;
-		}
+			});
 
-	};
+			var timeList = new TimeList;
 
-});
+			var TimeView = Backbone.View.extend({
+				template : Templating.extract($(template), "item"),
+
+				events : {
+					"click [data-action='edit']" : "edit"
+				},
+
+				initialize : function() {
+					this.setElement(this.template.clone());
+
+					this.listenTo(this.model, 'change', this.render);
+
+				},
+
+				render : function() {
+					Templating.render(this.$el, this.model.toJSON());
+					return this;
+
+				},
+				
+				edit : function() {
+					this.trigger("edit", this);
+					
+				}
+				
+			});
+
+			var TimeListView = Backbone.View.extend({
+
+				template : Templating.extract($(template), "timetable"),
+				
+				events : {
+					"click [data-action='save']" : "save"
+				},
+
+				initialize : function() {
+					this.listenTo(timeList, 'add', this.addOne);
+					this.listenTo(timeList, 'all', this.render);
+
+					this.setElement(this.template.clone());
+
+					$("#timetable").replaceWith(this.$el);
+					
+					this.form = Templating.find(this.$el, "timetable-form");
+
+					timeList.fetch();
+
+				},
+
+				render : function() {
+					Templating.render(this.$el, {
+						items : []
+					});
+
+				},
+
+				addOne : function(time) {
+					var view = new TimeView({
+						model : time
+					});
+					
+					this.listenTo(view, 'edit', this.edit);
+
+					Templating.append(this.$el, "items", view.render().$el);
+
+				},
+				
+				edit : function(time) {
+					Templating.render(this.form, time.model.toJSON());
+					
+				},
+				
+				save : function(ooo) {
+					console.log(ooo);
+				}
+
+			});
+
+			return {
+				init : function() {
+					return new TimeListView;
+				}
+
+			};
+
+		});
